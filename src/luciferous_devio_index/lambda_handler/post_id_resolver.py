d7c8ee8da093f3df3d74c22ff85520ff48edaa35
sinofseven <em.s.00001@gmail.com>
2024-02-03 00:31:36 +0900

    test parse slug

--- src/luciferous_devio_index/lambda_handler/post_id_resolver.py
+++ src/luciferous_devio_index/lambda_handler/post_id_resolver.py
@@ -17,10 +17,21 @@ logger = MyLogger(__name__)
 def handler(event: dict, _context):
     env = load_environment(class_dataclass=EnvironmentVariables)
     url = parse_url(event=event)
-    logger.info("url", url=url)
+    slug = parse_slug(url=url)
+    logger.info("url and slug", url=url, slug=slug)
 
 
+@logger.logging_function()
 def parse_url(*, event: dict) -> str:
     if (url := event.get("url")) is not None:
         return url
     return event["Records"][0]["body"]
+
+
+@logger.logging_function()
+def parse_slug(*, url: str) -> str:
+    part = url.split("/")
+    if url[-1] == "/":
+        return part[-2]
+    else:
+        return part[-1]
