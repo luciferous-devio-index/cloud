9fba95b22ccd372863023803933cbdcdca18e999
sinofseven <em.s.00001@gmail.com>
2024-02-03 00:14:06 +0900

    update feed dispatcher

--- src/luciferous_devio_index/lambda_handler/feed_dispatcher.py
+++ src/luciferous_devio_index/lambda_handler/feed_dispatcher.py
@@ -1,11 +1,13 @@
 from dataclasses import dataclass
-from typing import List
+from typing import List, Set
+from uuid import uuid4
 
 import feedparser
 from boto3.dynamodb.conditions import Attr
 from mypy_boto3_dynamodb import DynamoDBClient, DynamoDBServiceResource
+from mypy_boto3_sqs import SQSClient
 
-from luciferous_devio_index.common.aws import create_client, create_resource
+from luciferous_devio_index.common.aws import create_client
 from luciferous_devio_index.common.dataclasses import load_environment
 from luciferous_devio_index.common.logger import MyLogger
 
@@ -13,29 +15,18 @@ from luciferous_devio_index.common.logger import MyLogger
 @dataclass
 class EnvironmentVariables:
     url_feed: str
-    ddb_table_name: str
+    queue_url: str
 
 
 logger = MyLogger(__name__)
 
 
 @logger.logging_handler(with_return=False)
-def handler(
-    _event: dict,
-    _context,
-    ddb_client: DynamoDBClient = create_client("dynamodb"),
-    ddb_resource: DynamoDBServiceResource = create_resource("dynamodb"),
-):
+def handler(_event: dict, _context, client_sqs: SQSClient = create_client("sqs")):
     env = load_environment(class_dataclass=EnvironmentVariables)
     entries = get_feed_entries(url=env.url_feed)
-    list_post_id = parse_post_id(entries=entries)
-    for post_id in list_post_id:
-        put_post_id(
-            post_id=post_id,
-            table_name=env.ddb_table_name,
-            ddb_client=ddb_client,
-            ddb_resource=ddb_resource,
-        )
+    list_url = parse_url(entries=entries)
+    send_messages(list_url=list_url, queue_url=env.queue_url, client=client_sqs)
 
 
 @logger.logging_function()
@@ -63,3 +54,32 @@ def put_post_id(
         )
     except ddb_client.exceptions.ConditionalCheckFailedException:
         pass
+
+
+@logger.logging_function(write_log=False)
+def parse_url(*, entries: List[dict]) -> List[str]:
+    return [x["link"] for x in entries]
+
+
+@logger.logging_function(write_log=True, with_arg=True)
+def send_messages(*, list_url: List[str], queue_url: str, client: SQSClient):
+    union_succeeded_id: Set[str] = set()
+    map_url = {str(uuid4()): x for x in list_url}
+
+    while len(union_succeeded_id) != len(map_url):
+        entries = []
+        for message_id, url in map_url.items():
+            if len(entries) == 10:
+                break
+            if message_id in union_succeeded_id:
+                continue
+            entries.append({"Id": message_id, "MessageBody": url})
+
+        option = {"QueueUrl": queue_url, "Entries": entries}
+        logger.debug("send message batch", option=option)
+        resp = client.send_message_batch(**option)
+
+        union_succeeded_id |= set([x["Id"] for x in resp["Successful"]])
+
+        for item in resp["Failed"]:
+            logger.warning("send failed item", item=item)
